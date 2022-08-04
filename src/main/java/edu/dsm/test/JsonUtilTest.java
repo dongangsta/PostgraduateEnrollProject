44824650d4590599f5805fd4a3727593e58086ee
dongsimiao <dsmdsmdsm@foxmail.com>
2022-08-04 17:35:04 +0800

    添加hutool的JsonUtil测试类注释

--- src/main/java/edu/dsm/test/JsonUtilTest.java
+++ src/main/java/edu/dsm/test/JsonUtilTest.java
@@ -3,7 +3,15 @@ package edu.dsm.test;
 import cn.hutool.json.JSONArray;
 import cn.hutool.json.JSONUtil;
 
+/**
+ * 测试JsonUtil from hutool
+ */
 public class JsonUtilTest {
+    /**
+     * 测试功能入口
+     *
+     * @param args the input arguments
+     */
     public static void main(String[] args) {
         String jsonStr = "[\"value1\", \"value2\", \"value3\"]";
         JSONArray array = JSONUtil.parseArray(jsonStr);
