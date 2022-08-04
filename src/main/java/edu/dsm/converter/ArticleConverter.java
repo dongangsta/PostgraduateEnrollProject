e314de3bd8e5d91dc32b7e3db5e28ffb06272ebf
dongsimiao <dsmdsmdsm@foxmail.com>
2022-08-04 10:47:25 +0800

    添加转换器注释

--- src/main/java/edu/dsm/converter/ArticleConverter.java
+++ src/main/java/edu/dsm/converter/ArticleConverter.java
@@ -5,9 +5,21 @@ import edu.dsm.entity.vo.ArticleForShow;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
+/**
+ * The interface Article converter.
+ */
 @Mapper
 public interface ArticleConverter {
+    /**
+     * ArticleConverter实例，供其他类调用使用
+     */
     ArticleConverter INSTANCT = Mappers.getMapper(ArticleConverter.class);
 
+    /**
+     * 将article转换成前台显示的样式
+     *
+     * @param article 转换前的
+     * @return the article for show
+     */
     ArticleForShow conver(Article article);
 }
